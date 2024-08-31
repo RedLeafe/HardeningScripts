15a30950e7505ad31e6c483ebbc7658394d13973
Ryan Miller <miller.ryan2005@gmail.com>
2024-08-31 12:33:45 -0700

    IM DYING, ITS OVER

--- Linux/Hardening/password.sh
+++ Linux/Hardening/password.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Pw: " password
+read -p "Pw: " pass
 
 CHANGEPASSWORD() {
 	BIN=$( which chpasswd || which passwd )
--- Windows/Hardening/Firewall.ps1
+++ Windows/Hardening/Firewall.ps1
@@ -9,5 +9,9 @@ Disable-NetFirewallRule -Name (Get-NetFirewallRule | select -ExpandProperty Name
 # Default Settings
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 Set-NetFirewallProfile -DefaultOutboundProfile Block
+Set-NetFirewallProfile -DefaultInboundProfile Block
 
-# New-NetFirewallProfile -DisplayName "bruh" -Direction Outbound -Protocol TCP -LocalPort ___ -Action Allow
+New-NetFirewallRule -DisplayName “HTTP” -Direction Outbound -Protocol TCP -LocalPort 80 -Action Allow
+New-NetFirewallRule -DisplayName “DNS” -Direction Outbound -Protocol TCP -LocalPort 53 -Action Allow
+New-NetFirewallRule -DisplayName “SMB1” -Direction Outbound -Protocol TCP -LocalPort 445 -Action Allow
+New-NetFirewallRule -DisplayName “SMB2” -Direction Outbound -Protocol TCP -LocalPort 135 -Action Allow
